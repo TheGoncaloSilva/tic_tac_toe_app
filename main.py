3cd2daac6c8d6cb934584708c6a3cded2ff53b30
Gonçalo Silva <goncalo.lslv.silva@gmail.com>
2021-09-18 00:54:18 +0100

    Small improvements, added images to back and restore button and start os analyze_move function, to determine if the game has been won or if the options have ran out

--- main.py
+++ main.py
@@ -44,10 +44,12 @@ sm.current = "main"
 class MyApp(App):
 
     game_mode = [] # mode wich the game is supposed to be played
+    table = []
     player1 = "X" # Player 1 always exists
     player2 = "O" # Player 2 is a bot in single player mode and another player in multiplayer
-    active_player = 2 # Currently active player
-
+    active_player = 2 # Currently active player 
+# ****************************************
+    
     def build(self):
         self.title = 'Tic Tac Toe'
         return sm
@@ -61,15 +63,22 @@ class MyApp(App):
     def choose_pos(self, asset): # when a player pressed a button
         if asset.text == "" and (self.player1 != "" and self.player2 != ""):
             if self.active_player % 2 == 0:
-                asset.text = self.player1
+                asset.text = self.player2
+                self.active_player = 1
             else: 
-                asset.text = self.player2   
+                asset.text = self.player1
+                self.active_player = 2   
+
+            self.analyze_moves() # a move has been done, figure out if someone has won
+
         else: # If the conditions aren't met, it will give a warning to the user
             self.popup_manager(False, 'OOPS! :(', 'An Error occurred trying to register your position. Please reset the game or try again later')
     
     def choose_player(self): # Choose the player to start the game
         self.active_player = random.randrange(0, 2)
 
+    def restart(self):
+        print("restart iniciated")
 
     def clear_table(self): # Reset the game
         self.root.ids.gbtn_1.text = ""
@@ -83,11 +92,35 @@ class MyApp(App):
         self.root.ids.gbtn_9.text = ""
         self.choose_player()
 
-    def analyze_moves():
-        pass
+    def read_table(self): # Improve this
+        self.table[0][0] = self.root.ids.gbtn_1.text
+        self.table[0][1] = self.root.ids.gbtn_2.text
+        self.table[0][2] = self.root.ids.gbtn_3.text
+        self.table[1][0] = self.root.ids.gbtn_4.text
+        self.table[0][1] = self.root.ids.gbtn_5.text
+        self.table[0][2] = self.root.ids.gbtn_6.text
+        self.table[2][0] = self.root.ids.gbtn_7.text
+        self.table[0][1] = self.root.ids.gbtn_8.text
+        self.table[0][2] = self.root.ids.gbtn_9.text
 
-    def game_ended():
-        pass
+    def analyze_moves(self):
+
+        self.read_table() # update the table positions
+
+        if self.analyze_winner()[0]: # discover if a winner exists
+            print("game ended")
+
+
+
+    def analyze_winner(self):
+        value = 0
+        # player1.value = 1
+        # player2.value = -1
+        for l in range(3):
+            for c in range(3):
+                print("sum of them all")
+
+        return [True, 1]
 
     # Flexible way of showing Popups, parameters:
     #   - self...
@@ -115,7 +148,7 @@ class MyApp(App):
             pop = Popup(title=title, content=boxl, size_hint=(0.75,0.8))
             document = Label(text=message,markup=True, valign='top')
             button = Button(text='Dismiss', size_hint_y=None, height=40)
-            button2 = Button(text="Reset Game", size_hint_y=None, height=40)
+            button2 = Button(text="Restart", size_hint_y=None, height=40)
             button.bind(on_press=(lambda x:pop.dismiss()))
             boxl.add_widget(document)
             boxl2.add_widget(button)
